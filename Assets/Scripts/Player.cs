6610eccd848964a8eb8bbe93ef5c4824ce6167c5
John <yangtianzhuo970425@gmail.com>
2023-07-25 19:57:55 +1200

    Implementing New Input System
    Integrating the New Input System for Improved Input Handling and Control in the Application

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update()
     {
-        var inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-            inputVector.y = +1;
-        if (Input.GetKey(KeyCode.A))
-            inputVector.x = -1;
-        if (Input.GetKey(KeyCode.S))
-            inputVector.y = -1;
-        if (Input.GetKey(KeyCode.D))
-            inputVector.x = +1;
-
-        inputVector = inputVector.normalized;
+        var inputVector = gameInput.GetMovementVectorNormalized();
 
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
